02824594b08b531f66e35434f7140b0d59845ad3
Filipe Simões <lfcars@gmail.com>
2017-07-09 20:55:32 +0100

    second commit

--- app.js
+++ app.js
@@ -146,6 +146,13 @@ app.get('/vendas', function(req,res){
 // });
 
 
-app.listen(8080);
+//app.listen(8080);
 
-console.log('listening on 8080')
+  var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+  var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+  app.listen(server_port, server_ip_address, function(){
+    console.log("Listening on " + server_ip_address + ", server_port " + server_port)
+  });
+
+//console.log('listening on ?')
