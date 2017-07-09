3db4ddcb3c836fa1a3b3c2dfd09be3fdb402f486
Filipe Simões <lfcars@gmail.com>
2017-07-10 00:06:46 +0100

    e

--- app.js
+++ app.js
@@ -31,21 +31,16 @@ app.use(bodyParser.urlencoded({
 
 
 app.get('/', function(req,res){
-    if (err) {
-      res.send('error occured')
-    }
-    else {
       res.send('ok');
-    }
   });
 
-//app.listen(8080);
+app.listen(8080);
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
-
-  app.listen(server_port, server_ip_address, function(){
-    console.log("Listening on " + server_ip_address + ", server_port " + server_port)
-  });
+// var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+// var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+//
+//   app.listen(server_port, server_ip_address, function(){
+//     console.log("Listening on " + server_ip_address + ", server_port " + server_port)
+//   });
 
   app.use(express.static('public'));
