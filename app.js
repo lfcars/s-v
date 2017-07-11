73036dea34be6e8c74c1a3364be229c269380cd4
Filipe Simões <lfcars@gmail.com>
2017-07-11 20:20:23 +0100

    d

--- app.js
+++ app.js
@@ -35,11 +35,13 @@ app.get('/', function(req,res){
 
 //app.listen(8080);
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var server_port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
   app.listen(server_port, server_ip_address, function(){
     console.log("Listening on " + server_ip_address + ", server_port " + server_port)
   });
+
+module.exports = app ;
 
 //  app.use(express.static('public'));
